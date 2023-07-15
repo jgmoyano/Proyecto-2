58375a9662e681432e8b687f3bc3ea632b9e0962
Juan Moyano Anfossi <@jgmoyano>
2023-07-15 17:36:24 -0400

    Segunda versión subida del CRUD

--- script.js
+++ script.js
@@ -1,63 +1,90 @@
-function showData(){
-    let usersList;
-    if(localStorage.getItem("usersList") === null){
-        usersList = []
-    } else {
+function getUserList() {
+    let usersList = [];
+    if (localStorage.getItem("usersList") !== null) {
         usersList = JSON.parse(localStorage.getItem("usersList"))
     }
+
+    return usersList;
+}
+
+function showData() {
+    const usersList = getUserList();
+
     let html = "";
     usersList.forEach((user, index) => {
-        html += `<li>${user.name} ${user.email}<button onclick="deleteData(${index})">Eliminar</button><button onclick="editData(${index})">Editar</button></li>`
+        html += `
+                <tr>
+                    <td>${user.nombres}</td>
+                    <td>${user.apellidos}</td>
+                    <td>${user.edad}</td>
+                    <td>${user.email}</td>
+                    <td>
+                        <button onclick="deleteData(${index})">Eliminar</button>
+                        <button onclick="editData(${index})">Editar</button>
+                    </td>
+                </tr>
+                `
     });
-    document.querySelector('ul').innerHTML = html;
+    document.querySelector('table tbody').innerHTML = html;
 }
 
 document.onload = showData()
 
-function addData(event){
+function addData(event) {
     event.preventDefault();
-    let name = document.querySelector('#name').value;
+    let nombres = document.querySelector('#nombres').value;
+    let apellidos = document.querySelector('#apellidos').value;
+    let edad = document.querySelector('#edad').value;
     let email = document.querySelector('#email').value;
 
-    if (name === "" || email === "") return;
+    if (nombres === "" || apellidos === "" || edad === "" || email === "") return;
 
-    let usersList;
-    if(localStorage.getItem("usersList") === null){
-        usersList = []
-    } else {
-        usersList = JSON.parse(localStorage.getItem("usersList"))
-    }
-    usersList.push({name, email})
+    const usersList = getUserList();
+
+    usersList.push({ nombres, apellidos, edad, email })
     localStorage.setItem("usersList", JSON.stringify(usersList))
     showData()
 
-    document.querySelector('#name').value = ""
+    document.querySelector('#nombres').value = ""
+    document.querySelector('#apellidos').value = ""
+    document.querySelector('#edad').value = ""
     document.querySelector('#email').value = ""
 }
 
-function editData(index){
+function editData(index) {
     document.getElementById('add-btn').style.display = 'none';
     document.getElementById('edit-btn').style.display = 'block';
 
-    let usersList;
-    if(localStorage.getItem("usersList") === null){
-        usersList = []
-    } else {
-        usersList = JSON.parse(localStorage.getItem("usersList"))
-    }
-    document.querySelector('#name').value = usersList[index].name;
+    const usersList = getUserList();
+
+
+    document.querySelector('#nombres').value = usersList[index].nombres;
+    document.querySelector('#apellidos').value = usersList[index].apellidos;
+    document.querySelector('#edad').value = usersList[index].edad;
     document.querySelector('#email').value = usersList[index].email;
 
-    document.getElementById('edit-btn').onclick = function (){
-        usersList[index].name = document.querySelector('#name').value
+    document.getElementById('edit-btn').onclick = function () {
+        usersList[index].nombres = document.querySelector('#nombres').value
+        usersList[index].apellidos = document.querySelector('#apellidos').value
+        usersList[index].edad = document.querySelector('#edad').value
         usersList[index].email = document.querySelector('#email').value
 
         localStorage.setItem("usersList", JSON.stringify(usersList));
         showData();
-        document.querySelector('#name').value = ""
+
+        document.querySelector('#nombres').value = ""
+        document.querySelector('#apellidos').value = ""
+        document.querySelector('#edad').value = ""
         document.querySelector('#email').value = ""
 
         document.getElementById('add-btn').style.display = 'block';
         document.getElementById('edit-btn').style.display = 'none';
     }
+}
+
+function deleteData(index) {
+    const usersList = getUserList();
+    usersList.splice(index);
+    localStorage.setItem("usersList", JSON.stringify(usersList));
+    showData();
 }
