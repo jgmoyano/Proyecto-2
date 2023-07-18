b463b709385b6ddec11e75f0dbb8967558b7d7c6
Juan Moyano Anfossi <@jgmoyano>
2023-07-18 14:15:07 -0400

    Se corrigen problemas en el Script

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function getUserList() {
-    let usersList = [];
-    if (localStorage.getItem("usersList") !== null) {
-        usersList = JSON.parse(localStorage.getItem("usersList"))
-    }
+  let usersList = [];
+  if (localStorage.getItem("usersList") !== null) {
+    usersList = JSON.parse(localStorage.getItem("usersList"));
+  }
 
-    return usersList;
+  return usersList;
 }
 
 function showData() {
-    const usersList = getUserList();
+  const usersList = getUserList();
 
-    let html = "";
-    usersList.forEach((user, index) => {
-        html += `
+  let html = "";
+  usersList.forEach((user, index) => {
+    html += `
                 <tr>
                     <td>${user.nombres}</td>
                     <td>${user.apellidos}</td>
@@ -23,68 +23,85 @@ function showData() {
                         <button onclick="editData(${index})">Editar</button>
                     </td>
                 </tr>
-                `
-    });
-    document.querySelector('table tbody').innerHTML = html;
+                `;
+  });
+  document.querySelector("table tbody").innerHTML = html;
 }
 
-document.onload = showData()
+document.onload = showData();
 
 function addData(event) {
-    event.preventDefault();
-    let nombres = document.querySelector('#nombres').value;
-    let apellidos = document.querySelector('#apellidos').value;
-    let edad = document.querySelector('#edad').value;
-    let email = document.querySelector('#email').value;
+  event.preventDefault();
+  
+  let nombres = document.querySelector("#nombres").value;
+  let apellidos = document.querySelector("#apellidos").value;
+  let edad = document.querySelector("#edad").value;
+  let email = document.querySelector("#email").value;
 
-    if (nombres === "" || apellidos === "" || edad === "" || email === "") return;
+  
+  if (nombres === "" || apellidos === "" || edad === "" || email === "") {
+    alert("Faltan datos por completar!");
+    return;
+  }
 
-    const usersList = getUserList();
+  const usersList = getUserList();
 
-    usersList.push({ nombres, apellidos, edad, email })
-    localStorage.setItem("usersList", JSON.stringify(usersList))
-    showData()
+  usersList.push({ nombres, apellidos, edad, email });
+  localStorage.setItem("usersList", JSON.stringify(usersList));
+  showData();
 
-    document.querySelector('#nombres').value = ""
-    document.querySelector('#apellidos').value = ""
-    document.querySelector('#edad').value = ""
-    document.querySelector('#email').value = ""
+  document.querySelector("#nombres").value = "";
+  document.querySelector("#apellidos").value = "";
+  document.querySelector("#edad").value = "";
+  document.querySelector("#email").value = "";
 }
 
 function editData(index) {
-    document.getElementById('add-btn').style.display = 'none';
-    document.getElementById('edit-btn').style.display = 'block';
+  document.getElementById("add-btn").style.display = "none";
+  document.getElementById("edit-btn").style.display = "block";
 
-    const usersList = getUserList();
+  const usersList = getUserList();
 
+  document.querySelector("#nombres").value = usersList[index].nombres;
+  document.querySelector("#apellidos").value = usersList[index].apellidos;
+  document.querySelector("#edad").value = usersList[index].edad;
+  document.querySelector("#email").value = usersList[index].email;
 
-    document.querySelector('#nombres').value = usersList[index].nombres;
-    document.querySelector('#apellidos').value = usersList[index].apellidos;
-    document.querySelector('#edad').value = usersList[index].edad;
-    document.querySelector('#email').value = usersList[index].email;
+  document.getElementById("edit-btn").onclick = function () {
+    
+    let nombres = document.querySelector("#nombres").value;
+    let apellidos = document.querySelector("#apellidos").value;
+    let edad = document.querySelector("#edad").value;
+    let email = document.querySelector("#email").value;
 
-    document.getElementById('edit-btn').onclick = function () {
-        usersList[index].nombres = document.querySelector('#nombres').value
-        usersList[index].apellidos = document.querySelector('#apellidos').value
-        usersList[index].edad = document.querySelector('#edad').value
-        usersList[index].email = document.querySelector('#email').value
-
-        localStorage.setItem("usersList", JSON.stringify(usersList));
-        showData();
-
-        document.querySelector('#nombres').value = ""
-        document.querySelector('#apellidos').value = ""
-        document.querySelector('#edad').value = ""
-        document.querySelector('#email').value = ""
-
-        document.getElementById('add-btn').style.display = 'block';
-        document.getElementById('edit-btn').style.display = 'none';
+    
+    if (nombres === "" || apellidos === "" || edad === "" || email === "") {
+      alert("Faltan datos por completar!");
+      return;
     }
+
+    usersList[index].nombres = nombres;
+    usersList[index].apellidos = apellidos;
+    usersList[index].edad = edad;
+    usersList[index].email = email;
+
+    localStorage.setItem("usersList", JSON.stringify(usersList));
+    showData();
+
+    document.querySelector("#nombres").value = "";
+    document.querySelector("#apellidos").value = "";
+    document.querySelector("#edad").value = "";
+    document.querySelector("#email").value = "";
+
+    document.getElementById("add-btn").style.display = "block";
+    document.getElementById("edit-btn").style.display = "none";
+  };
 }
 
 function deleteData(index) {
-    const usersList = getUserList();
-    usersList.splice(index);
-    localStorage.setItem("usersList", JSON.stringify(usersList));
-    showData();
+  const usersList = getUserList();
+  
+  usersList.splice(index, 1);
+  localStorage.setItem("usersList", JSON.stringify(usersList));
+  showData();
 }
